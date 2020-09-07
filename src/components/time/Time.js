b0adb6e74a4f024329652a737587ad8acac67322
Van Hiep <vanhiep99w@gmail.com>
2020-09-07 15:10:21 +0700

    create time component

--- src/components/time/Time.js
+++ src/components/time/Time.js
@@ -1,13 +1,56 @@
 import React from "react";
+import "./Time.css";
+import { Field, reduxForm } from "redux-form";
+import PlayCircleFilledWhiteIcon from "@material-ui/icons/PlayCircleFilledWhite";
 
-const Time = () => {
-  return (
-    <div className="time">
-      <div className="time_description"></div>
-      <div className="time_task"></div>
-      <div className="time_track"></div>
-    </div>
-  );
-};
+class Time extends React.Component {
+  renderInput = ({ name, type, placeholder, className, readOnly = false }) => {
+    return (
+      <input
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        className={className}
+        readOnly={readOnly}
+      />
+    );
+  };
 
-export default Time;
+  render() {
+    return (
+      <form className="form">
+        <Field
+          name="description"
+          type="text"
+          component={this.renderInput}
+          placeholder="Description..."
+          className="form__input form__input__description"
+        />
+        <Field
+          name="task "
+          type="text"
+          component={this.renderInput}
+          placeholder="Task..."
+          className="form__input form__input__task"
+        />
+
+        <Field
+          name="time "
+          type="text"
+          component={this.renderInput}
+          placeholder="0:00:00"
+          className="form__input form__input__time"
+          readOnly
+        />
+
+        <button className="form__button">
+          <PlayCircleFilledWhiteIcon />
+        </button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "trackTimeForm",
+})(Time);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers";
+
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
+
+// {
+//   /* <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>, */
+// }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
