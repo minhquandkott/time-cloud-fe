e390d3e287f01d053194ff77aba8e6b465c37b70
Van Hiep <vanhiep99w@gmail.com>
2020-09-08 15:25:57 +0700

    create auth reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,13 @@
-const initialState = {
+import {
+  AUTH_AUTHENTICATION,
+  AUTH_FAIL,
+  AUTH_START,
+  AUTH_SUCCESS,
+  AUTH_LOGOUT,
+  AUTH_SET_REDIRECT_PATH,
+} from "../actions/actionType";
+
+const state = {
   token: null,
   userId: null,
   loading: false,
@@ -6,4 +15,38 @@ const initialState = {
   authRedirectPath: "/",
 };
 
-export default (state, action) => {};
+export default (state, { type, payload }) => {
+  switch (type) {
+    case AUTH_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case AUTH_SUCCESS:
+      return {
+        ...state,
+        token: payload.token,
+        userId: payload.userId,
+      };
+
+    case AUTH_FAIL:
+      return {
+        ...state,
+        error: payload,
+      };
+
+    case AUTH_SET_REDIRECT_PATH:
+      return {
+        ...state,
+        authRedirectPath: payload,
+      };
+
+    case AUTH_LOGOUT:
+      return {
+        ...state,
+      };
+
+    default:
+      return state;
+  }
+};
