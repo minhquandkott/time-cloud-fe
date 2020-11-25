9bf50546186497b0700dade83585b03217895240
Quan T. M. Huynh <=>
2020-11-25 14:49:57 +0700

    api

---
+++ src/components/changeTypeDiscussion/ChangeTypeDiscussion.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';
+import './ChangeTypeDiscussion.css';
+
+
+const ChangeTypeDiscussion = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChangeTypeDiscussion;
